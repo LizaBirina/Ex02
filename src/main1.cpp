848617d431e99da68e8081b32cfe568fd3fbd8d5
LizaBirina <71217634+LizaBirina@users.noreply.github.com>
2020-10-11 22:00:02 +0300

    Add files via upload

---
+++ src/main1.cpp
@@ -0,0 +1,21 @@
+#include "task1.h"
+#include <stdio.h>
+
+int main() {
+	double result = calc(2.0);
+	printf("%lf\n", result);
+
+	double average(double x, double y);
+	printf("%lf\n", result);
+
+	bool good(double guess, double x);
+	printf("%lf\n", result);
+
+	double improve(double guess, double x);
+	printf("%lf\n", result);
+
+	double iter(double guess, double x);
+	printf("%lf\n", result);
+
+	return 0;
+}

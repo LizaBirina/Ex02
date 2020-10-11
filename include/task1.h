c0b87a27e83c379e16074d0a945531aa9c16f6f2
LizaBirina <71217634+LizaBirina@users.noreply.github.com>
2020-10-11 22:12:10 +0300

    Add files via upload

---
+++ include/task1.h
@@ -0,0 +1,11 @@
+#ifndef TASK_H
+#define TASK_H
+
+double average(double x, double y);
+bool good(double guess, double x);
+double improve(double guess, double x);
+double iter(double guess, double x);
+double calc(double arg);
+
+#endif 
+
